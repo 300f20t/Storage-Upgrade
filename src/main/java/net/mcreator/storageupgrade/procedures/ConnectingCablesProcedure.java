7d882c8721361b1520498281cce1803e43d305ea
300f20t <93136063+300f20t@users.noreply.github.com>
2025-07-30 23:32:40 +0300

    optimize cables

--- src/main/java/net/mcreator/storageupgrade/procedures/ConnectingCablesProcedure.java
+++ src/main/java/net/mcreator/storageupgrade/procedures/ConnectingCablesProcedure.java
@@ -309,52 +309,7 @@ public class ConnectingCablesProcedure {
 					}
 				}
 			}
-		} else if (3 == wireDirections.size()) {
-			{
-				BlockPos _bp = BlockPos.containing(x, y, z);
-				BlockState _bs = StorageUpgradeModBlocks.LASER_ROUTER.get().defaultBlockState();
-				BlockState _bso = world.getBlockState(_bp);
-				for (Property<?> _propertyOld : _bso.getProperties()) {
-					Property _propertyNew = _bs.getBlock().getStateDefinition().getProperty(_propertyOld.getName());
-					if (_propertyNew != null && _bs.getValue(_propertyNew) != null)
-						try {
-							_bs = _bs.setValue(_propertyNew, _bso.getValue(_propertyOld));
-						} catch (Exception e) {
-						}
-				}
-				world.setBlock(_bp, _bs, 3);
-			}
-		} else if (4 == wireDirections.size()) {
-			{
-				BlockPos _bp = BlockPos.containing(x, y, z);
-				BlockState _bs = StorageUpgradeModBlocks.LASER_ROUTER.get().defaultBlockState();
-				BlockState _bso = world.getBlockState(_bp);
-				for (Property<?> _propertyOld : _bso.getProperties()) {
-					Property _propertyNew = _bs.getBlock().getStateDefinition().getProperty(_propertyOld.getName());
-					if (_propertyNew != null && _bs.getValue(_propertyNew) != null)
-						try {
-							_bs = _bs.setValue(_propertyNew, _bso.getValue(_propertyOld));
-						} catch (Exception e) {
-						}
-				}
-				world.setBlock(_bp, _bs, 3);
-			}
-		} else if (5 == wireDirections.size()) {
-			{
-				BlockPos _bp = BlockPos.containing(x, y, z);
-				BlockState _bs = StorageUpgradeModBlocks.LASER_ROUTER.get().defaultBlockState();
-				BlockState _bso = world.getBlockState(_bp);
-				for (Property<?> _propertyOld : _bso.getProperties()) {
-					Property _propertyNew = _bs.getBlock().getStateDefinition().getProperty(_propertyOld.getName());
-					if (_propertyNew != null && _bs.getValue(_propertyNew) != null)
-						try {
-							_bs = _bs.setValue(_propertyNew, _bso.getValue(_propertyOld));
-						} catch (Exception e) {
-						}
-				}
-				world.setBlock(_bp, _bs, 3);
-			}
-		} else if (6 == wireDirections.size()) {
+		} else {
 			{
 				BlockPos _bp = BlockPos.containing(x, y, z);
 				BlockState _bs = StorageUpgradeModBlocks.LASER_ROUTER.get().defaultBlockState();
